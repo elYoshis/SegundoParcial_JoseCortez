aeacce70723bb19f02a2a3e83f895692e8eb3d15
JoseCortez <xxabyssxx@gmail.com>
2025-10-27 23:38:18 -0400

    feat(web):crear servicio empleado y consumir API

--- src/app/core/services/empleado-services.ts
+++ src/app/core/services/empleado-services.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,4 +9,9 @@ export class EmpleadoServices {
   private base = 'http://localhost:8000/api/empleados';
 
   constructor(private http:HttpClient) { }
+
+  listaEmpleados():Observable<any[]>{
+  return this.http.get<any[]>(this.base);
 }
+}
+
